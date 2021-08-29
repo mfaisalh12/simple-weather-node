9dc59bc483857cafdc228c73691238edf1cb570e
Faisal <mfaisalhidayatullah@gmail.com>
2021-08-29 09:18:54 +0700

    feat: add ejs content

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const https = require('https');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	res.render('index');
 });
 
 app.post('/', (req, res) => {
 	const query = req.body.cityName;
 	const apiKey = '1c61f0cacaa3c4dbeb99d25cb0158b9b';
-	const unit = 'matric';
+	const unit = 'metric';
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
 
 	https
@@ -27,20 +28,13 @@ app.post('/', (req, res) => {
 				const icon = weatherData.weather[0].icon;
 				const imageURL =
 					'http://openweathermap.org/img/wn/' + icon + '@2x.png';
-				res.write(
-					'<p>The Weather is currently ' +
-						weatherDescription +
-						'</p>',
-				);
-				res.write('<img src=' + imageURL + '>');
-				res.write(
-					'<h1>The temperature in ' +
-						query +
-						' is ' +
-						temp +
-						' degrees Celcius.</h1>',
-				);
-				res.send();
+
+				res.render('result', {
+					weatherDescription: weatherDescription,
+					image: imageURL,
+					degree: query,
+					temp: temp,
+				});
 			});
 		})
 		.on('error', (err) => {
